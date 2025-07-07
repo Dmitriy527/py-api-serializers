627b0b24056ba90282f2fd4b37a5ed1b8033d799
Dmytro <dimkanividimka4@gmail.com>
2025-07-07 19:36:16 +0300

    Solution

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -14,7 +14,7 @@ from cinema.models import (
 class CinemaHallSerializer(serializers.ModelSerializer):
     class Meta:
         model = CinemaHall
-        fields = "name", "rows", "seats_in_row", "capacity"
+        fields = "id", "name", "rows", "seats_in_row", "capacity"
         read_only_fields = ("id", "capacity")
 
 
