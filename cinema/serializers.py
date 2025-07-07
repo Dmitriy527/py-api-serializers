30c06160d5a6ab468413e7407fa43001e6fb914b
Dmytro <dimkanividimka4@gmail.com>
2025-07-07 19:29:10 +0300

    Solution

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -28,7 +28,7 @@ class GenreSerializer(serializers.ModelSerializer):
 class ActorSerializer(serializers.ModelSerializer):
     class Meta:
         model = Actor
-        fields = "__all__"
+        fields = "id", "first_name", "last_name", "full_name"
         read_only_fields = ("id", )
 
 
